3104bce65de9ef68314767556ae8a65f48987c13
unknown <ming29398@gmail.com>
2018-08-23 06:17:49 -0700

    display key enter and wrong guesses letter

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,53 +8,55 @@ var wordElement = document.getElementById('word-blanks');
 var winCounterElement = document.getElementById('win-counter');
 var lossCounterElement = document.getElementById('loss-counter');
 var guessesLeftElement = document.getElementById('guesses-left');
-var wrongguessElement = document.getElementById('wrong-guesses');
-//var userTextElement = document.getElementById('user-text');
+var wrongGuessElement = document.getElementById('wrong-guesses');
+var userTextElement = document.getElementById('user-text');
 
 function setUp() {
     totalGuessAllow = 10;
     var index = Math.floor(Math.random() * wordGame.length);
-    word =  wordGame[index];
-    console.log("word: "+word);
-    
+    word = wordGame[index];
+
     totalGuessAllow = 10;
     winCounter = 0;   // what is winCounter 
     lossCounter = 0;
     wrongGuesses = [];
     correctGuesses = [];
-  
+
     // initialize correctGuesses array with underscores
     for (var i = 0; i < word.length; i++) {
-      correctGuesses.push('_');
+        correctGuesses.push('_');
     }
-  
+
     console.log(correctGuesses);
     wordElement.innerHTML = correctGuesses.join(' ');
     //wordElement.innerHTML = correctGuesses;
     guessesLeftElement.innerHTML = totalGuessAllow;
-    wrongguessElement.innerHTML = lossCounter;
+    wrongGuessElement.innerHTML = wrongGuesses;
 }
 
 // Next, we give JavaScript a function to execute when onkeyup event fires.
 document.onkeyup = function (event) {
-    totalGuessAllow -= 1;
-    var letter =  String.fromCharCode(event.keyCode).toLowerCase();
-    if (word.indexOf(letter) === -1) { 
+    var letter = String.fromCharCode(event.keyCode).toLowerCase();
+    userTextElement.innerHTML = letter;
+    totalGuessAllow = totalGuessAllow - 1;
+    guessesLeftElement.innerHTML = totalGuessAllow;
+
+    if (word.indexOf(letter) === -1) {
         wrongGuesses.push(letter); // update letters guessed
-       // lettersGuessedElement.innerHTML = wrongGuesses.join(', ');
+        wrongGuessElement.innerHTML = wrongGuesses.join(', ');
         lossCounter += 1;
-       // lossCounterElement.innerHTML = lossCounter;
-      } else { // letter IS in the word
+        // lossCounterElement.innerHTML = lossCounter;
+    } else { // letter IS in the word
         // replace underscore with the letter
         for (var i = 0; i < word.length; i++) {
-          if (word[i] === letter) {
-            correctGuesses[i] = letter;
-          }
+            if (word[i] === letter) {
+                correctGuesses[i] = letter;
+            }
         }
-       // wrongguessElement.innerHTML = wrongCounter;
+        // wrongguessElement.innerHTML = wrongCounter;
         wordElement.innerHTML = correctGuesses.join(' ');
-      }
- 
+    }
+
 };
 
 setUp();
